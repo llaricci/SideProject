7acd2b91d48d2f808075cc5b0a3fc9c0cf05ce1b
Calvin526 <calvinmgarcia@hotmail.com>
2024-12-14 01:21:17 -0500

    Added dynamic project props to project page

---
+++ client/src/app/projects/[id]/page.jsx
@@ -0,0 +1,47 @@
+"use client"
+
+import Project from "@/app/components/Project";
+
+import { useQuery, useMutation } from "@apollo/client";
+import { useParams, redirect } from "next/navigation";
+
+import React from 'react';
+import ReactMarkdown from 'react-markdown';
+
+import queries from "../../queries";
+
+export default function ProjectPage()
+{
+    let { id } = useParams();
+
+    const { loading, error, data } = useQuery(queries.GetProjectById, 
+    {
+        fetchPolicy: "cache-and-network",
+        variables: { id: id },
+    });
+
+    if (loading) 
+    {
+        return <div>Loading...</div>;
+    }
+
+
+    if (error) 
+    {
+        redirect("/error");
+    } 
+    
+    else if (data) 
+    {
+        let project = data.getProjectById;
+        //let comments = project.comments;
+        
+        
+        
+        return (
+            <div>
+                <Project project={project} />
+            </div>
+          );
+    }
+}
--- client/src/app/projects/testproject/page.jsx
+++ client/src/app/projects/testproject/page.jsx
@@ -1,11 +1,70 @@
-import Project from "../../components/Project"
+import React from 'react';
+import ReactMarkdown from 'react-markdown';
 
-export default function ProjectPage()
+import Comment from '@/app/components/Comment';
+
+const markdownContent = `
+# Roommate Roulette 
+---
+## Technologies Used
+- HTML, CSS, Javascript
+
+---
+
+## Description
+- Roommate Roulette is a web-application designed to help students find roommate groups with others living in the same area.
+
+---
+
+## Number of Favorites
+- 2
+
+---  
+
+## Favorited By
+- Reshiram Dragon (reshiram@example.com)
+- Zekrom Dragon (zekrom@example.com)
+
+---
+## Comments
+
+`;
+
+let comment =
 {
+    user:{
+        _id: "testuser",
+        firstName: "Fuecoco",
+        lastName: "FireCroc",
+        email: "fuecoco@example.com"
+    },
+    comment: "Nice project! Great use of GraphQL and TailwindCSS!"
 
-    return (
-        <div>
-            <Project />
-        </div>
-    )
 }
+
+function Project() {
+    return (
+        <div className = "justify-items-center"
+            style={{ padding: '20px', fontFamily: 'Arial, sans-serif', lineHeight: '1.6'}}>
+            <div className="w-1/2">
+                <ReactMarkdown>{markdownContent}</ReactMarkdown>
+                <style>
+                    {` h1 { font-size: 2.5rem; font-weight: bold; }
+                    h2 { font-size: 2rem; font-weight: bold; }
+                    h3 { font-size: 1.75rem; font-weight: bold; }
+                    p { font-size: 1rem; }
+                    hr { border: none; border-top: 2px solid #ccc;  margin: 1.5rem 0; } `}
+                </style>
+                <div className="grid grid-cols-2 gap-2 mx-auto">
+                    <Comment comment = {comment}/>
+                    <Comment comment = {comment}/>
+                    <Comment comment = {comment}/>
+                    <Comment comment = {comment}/>
+                    
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Project;
