000bd259c299f1bd399a0fcb102cc83e8b0c6178
Andres Fernandez <94069766+Afernan413@users.noreply.github.com>
2024-12-13 20:20:25 -0500

    new homepage

--- client/src/app/components/Header.jsx
+++ client/src/app/components/Header.jsx
@@ -1,53 +1,49 @@
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
 
-import Link from 'next/link';
+import Link from "next/link";
 
+function Header() {
+  return (
+    <div>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              <Link className="text-3xl font-bold text-gray-800 mb-4" href="/">
+                SideProject
+              </Link>
+            </Typography>
 
-function Header()
-{
-    return (
-        <div>
-            <Box sx={{ flexGrow: 1 }}>
-                <AppBar position="static">
-                    <Toolbar>
-                    <IconButton
-                        size="large"
-                        edge="start"
-                        color="inherit"
-                        aria-label="menu"
-                        sx={{ mr: 2 }}
-                    >
-                        <MenuIcon />
-                    </IconButton>
-                    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                        SideProject
-                    </Typography>
-                    
-
-
-                    <Button color="inherit">View Favorites</Button>
-                    <br />
-                    <Button color="inherit">
-                        <Link href = "/users/testuser">
-                            View Test Profile
-                        </Link>
-                    </Button>
-                     <Button color="inherit">
-                        <Link href = "/projects/testproject">
-                            View Test Project
-                        </Link>
-                    </Button>
-                    </Toolbar>
-                </AppBar>
-                </Box>
-        </div>
-    )
+            <Button color="inherit">
+              <p className="text-[16px] font-bold text-gray-800">View Favorites</p>
+            </Button>
+            <br />
+            <Button color="inherit">
+              <Link href="/users/testuser"><p className="text-[16px] font-bold text-gray-800">View Test Profile</p></Link>
+            </Button>
+            <Button color="inherit">
+              <Link href="/projects/testproject"><p className="text-[16px] font-bold text-gray-800">View Test Project</p></Link>
+            </Button>
+          </Toolbar>
+        </AppBar>
+      </Box>
+    </div>
+  );
 }
 
 export default Header;
