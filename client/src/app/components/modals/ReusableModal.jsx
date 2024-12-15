c3a37cfa741fe2b630220542f1e18becdfe7ed5c
lianna lariccia <liannalariccia@gmail.com>
2024-12-15 12:42:06 -0500

    created reusable modal component and search page

---
+++ client/src/app/components/modals/ReusableModal.jsx
@@ -0,0 +1,57 @@
+
+import React from 'react';
+import { Modal, Box, TextField, Button } from '@mui/material';
+
+const ReusableModal = ({
+  open,
+  onClose,
+  title,
+  fields,
+  setFieldValues,
+  onSubmit,
+}) => {
+  const handleChange = (field, value) => {
+    setFieldValues((prev) => ({ ...prev, [field]: value }));
+  };
+
+  return (
+    <Modal open={open} onClose={onClose}>
+      <Box
+        sx={{
+          position: 'absolute',
+          top: '50%',
+          left: '50%',
+          transform: 'translate(-50%, -50%)',
+          width: '100%',
+          maxWidth: 400,
+          bgcolor: 'white',
+          p: 3,
+          boxShadow: 2,
+        }}
+      >
+        <h3>{title}</h3>
+        {fields.map((field) => (
+          <TextField
+            key={field.name}
+            label={field.label}
+            fullWidth
+            value={field.value}
+            onChange={(e) => handleChange(field.name, e.target.value)}
+            margin="normal"
+            type={field.type || 'text'}
+          />
+        ))}
+        <Button
+          onClick={onSubmit}
+          variant="contained"
+          color="primary"
+          sx={{ mt: 2 }}
+        >
+          Save
+        </Button>
+      </Box>
+    </Modal>
+  );
+};
+
+export default ReusableModal;
---
+++ client/src/app/search/page.jsx
@@ -0,0 +1,12 @@
+"use client";
+
+import SearchBar from "../components/SearchBar";
+
+
+const SearchBarPage = () => {
+    return (
+        <SearchBar />
+    );
+};
+
+export default SearchBarPage;
