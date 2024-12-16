b5eae379dea4fd937349fea0c2852713b4cef991
Andresfern413 <afernan6@stevens.edu>
2024-12-16 09:40:38 -0500

    updated search to give errors

--- client/src/app/components/Search.jsx
+++ client/src/app/components/Search.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useLazyQuery } from "@apollo/client";
 import { TextField, Button, MenuItem, Box, Typography } from "@mui/material";
 import { useRouter } from "next/navigation";
@@ -9,52 +9,108 @@ import {
   searchUserByName,
   GetProjectsbyTechnology,
 } from "../queries";
-
+const technologies = [
+  "JAVASCRIPT",
+  "PYTHON",
+  "JAVA",
+  "CSHARP",
+  "CPLUSPLUS",
+  "RUBY",
+  "PHP",
+  "TYPESCRIPT",
+  "SWIFT",
+  "KOTLIN",
+  "GO",
+  "RUST",
+  "HTML",
+  "CSS",
+  "SQL",
+  "GRAPHQL",
+  "NODE_JS",
+  "REACT",
+  "ANGULAR",
+  "VUE",
+  "NEXT_JS",
+  "SVELTE",
+  "TAILWINDCSS",
+  "BOOTSTRAP",
+  "AWS",
+  "GOOGLE_CLOUD",
+  "ORACLE_CLOUD",
+  "DOCKER",
+  "KUBERNETES",
+  "MONGODB",
+  "POSTGRESQL",
+  "REDIS",
+  "FIREBASE",
+  "GIT",
+  "GITHUB",
+  "OTHER",
+];
 const Search = () => {
   const router = useRouter();
   const [searchType, setSearchType] = useState("");
   const [queryInput, setQueryInput] = useState("");
-
+  const [error, setError] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
   const [projectsByTechnology, { data: projectsByTechData }] = useLazyQuery(
     GetProjectsbyTechnology
   );
   const [projectByName, { data: projectsData }] =
     useLazyQuery(searchProjectByName);
   const [userByName, { data: usersData }] = useLazyQuery(searchUserByName);
-
-  const handleSearch = () => {
-    console.log("searchType: ", searchType);
-    console.log("queryInput: ", queryInput);
+  const [results, setResults] = useState(<></>);
+  useEffect(() => {
+    if (searchResults.length === 0) {
+      setResults(<Typography>{"No results found"}</Typography>);
+    } else {
+      setResults(renderResults());
+    }
+    setError("");
+  }, [searchResults]);
+  const handleSearch = async () => {
+    setError("");
+    if (!queryInput) {
+      setResults(<></>);
+      setError("Please enter a search query");
+      return;
+    }
     if (searchType === "projectsByTechnology") {
-      projectsByTechnology({
+      await projectsByTechnology({
         variables: { technology: queryInput.toUpperCase() },
+      }).then((res) => {
+        setSearchResults(res.data.getProjectsByTechnology);
       });
     } else if (searchType === "projectByName") {
-      projectByName({ variables: { searchTerm: queryInput } });
+      await projectByName({ variables: { searchTerm: queryInput } }).then(
+        (res) => {
+          setSearchResults(res.data.searchProjectByName);
+        }
+      );
     } else if (searchType === "userByName") {
-      userByName({ variables: { searchTerm: queryInput } });
+      await userByName({ variables: { searchTerm: queryInput } }).then(
+        (res) => {
+          setSearchResults(res.data.searchUserByName);
+        }
+      );
     } else {
-      console.log("Invalid search type.");
+      setResults(<></>);
+      setError("Plesae select a search type");
     }
   };
-
   const renderResults = () => {
     if (
       searchType === "projectsByTechnology" &&
       projectsByTechData?.getProjectsByTechnology
     ) {
-      console.log(
-        "projectsByTechData: ",
-        projectsByTechData.getProjectsByTechnology
-      );
       return (
         <ul>
-          {projectsByTechData.getProjectsByTechnology.map((project) => (
+          {searchResults.map((project) => (
             <li
               key={project._id}
               onClick={() => router.push(`/projects/${project._id}`)}
             >
-              {project.name} - {project.technologies}
+              {project.name} - {project.technologies.join(",  ")}
             </li>
           ))}
         </ul>
@@ -62,10 +118,9 @@ const Search = () => {
     }
 
     if (searchType === "projectByName" && projectsData?.searchProjectByName) {
-      console.log("projectsData: ", projectsData);
       return (
         <ul>
-          {projectsData.searchProjectByName.map((project) => (
+          {searchResults.map((project) => (
             <li
               key={project._id}
               onClick={() => router.push(`/projects/${project._id}`)}
@@ -78,22 +133,19 @@ const Search = () => {
     }
 
     if (searchType === "userByName" && usersData?.searchUserByName) {
-      console.log("usersData: ", usersData);
       return (
         <ul>
-          {usersData.searchUserByName.map((user) => (
+          {searchResults.map((user) => (
             <li
               key={user._id}
               onClick={() => router.push(`/users/${user._id}`)}
             >
-              {user.firstName} {user.lastName.substring(0, 1)}
+              {user.firstName}-{user.lastName.substring(0, 1)}
             </li>
           ))}
         </ul>
       );
     }
-
-    return <Typography>No results found for {queryInput}</Typography>;
   };
 
   return (
@@ -101,30 +153,60 @@ const Search = () => {
       <Typography variant="h4" gutterBottom>
         Search
       </Typography>
+
       <Box sx={{ marginBottom: 2, backgroundColor: "#ffffff" }}>
         <TextField
           select
           label="Search Type"
           value={searchType}
-          onChange={(e) => setSearchType(e.target.value)}
+          onChange={(e) => {
+            setQueryInput("");
+            setSearchType(e.target.value);
+            setError("");
+          }}
           fullWidth
         >
           <MenuItem value="projectsByTechnology">
-            Projects by technology
+            Projects by Technology
           </MenuItem>
           <MenuItem value="projectByName">Projects by Name</MenuItem>
           <MenuItem value="userByName">User by Name</MenuItem>
         </TextField>
       </Box>
 
-      {searchType && (
+      {searchType && searchType != "projectsByTechnology" && (
         <TextField
           label="Search Query"
           value={queryInput}
-          onChange={(e) => setQueryInput(e.target.value)}
+          onChange={(e) => {
+            setQueryInput(e.target.value);
+            setError("");
+          }}
           fullWidth
+          required
           sx={{ marginBottom: 2, backgroundColor: "#ffffff" }}
         />
+      )}
+      {searchType && searchType === "projectsByTechnology" && (
+        <Box sx={{ marginBottom: 2, backgroundColor: "#ffffff" }}>
+          <TextField
+            select
+            label="Search Technology"
+            value={queryInput}
+            onChange={(e) => {
+              setQueryInput(e.target.value);
+              setError("");
+            }}
+            fullWidth
+            required
+          >
+            {technologies.map((tech) => (
+              <MenuItem key={tech} value={tech}>
+                {tech}
+              </MenuItem>
+            ))}
+          </TextField>
+        </Box>
       )}
 
       <Button variant="contained" onClick={handleSearch}>
@@ -133,7 +215,8 @@ const Search = () => {
 
       <Box sx={{ marginTop: 4 }}>
         <Typography variant="h6">Results:</Typography>
-        {renderResults()}
+        {error && <Typography color="error">{error}</Typography>}
+        {results}
       </Box>
     </Box>
   );
