93c30834b63a20e7def8fdc1fe9c3026851b8534
Andresfern413 <afernan6@stevens.edu>
2024-12-15 19:00:44 -0500

    fixed bugs that werent allowing search results to be displayed, ran prettier

--- client/src/app/components/Search.jsx
+++ client/src/app/components/Search.jsx
@@ -1,42 +1,59 @@
 "use client";
 
-import React, { useState } from 'react';
-import { useLazyQuery } from '@apollo/client';
-import { TextField, Button, MenuItem, Box, Typography } from '@mui/material';
-import { useRouter } from 'next/navigation';
-import { searchProjectByName, searchUserByName, GetProjectsbyTechnology } from '../queries';
+import React, { useState } from "react";
+import { useLazyQuery } from "@apollo/client";
+import { TextField, Button, MenuItem, Box, Typography } from "@mui/material";
+import { useRouter } from "next/navigation";
+import {
+  searchProjectByName,
+  searchUserByName,
+  GetProjectsbyTechnology,
+} from "../queries";
 
 const Search = () => {
   const router = useRouter();
-  const [searchType, setSearchType] = useState('');
-  const [queryInput, setQueryInput] = useState('');
+  const [searchType, setSearchType] = useState("");
+  const [queryInput, setQueryInput] = useState("");
 
-  const [projectsByTechnology, { data: projectsByTechData }] = useLazyQuery(GetProjectsbyTechnology);
-  const [projectByName, { data: projectsData }] = useLazyQuery(searchProjectByName);
+  const [projectsByTechnology, { data: projectsByTechData }] = useLazyQuery(
+    GetProjectsbyTechnology
+  );
+  const [projectByName, { data: projectsData }] =
+    useLazyQuery(searchProjectByName);
   const [userByName, { data: usersData }] = useLazyQuery(searchUserByName);
 
-
-
   const handleSearch = () => {
-    if (searchType === 'projectsByTechnology') {
-      projectsByTechnology({ variables: { technology: queryInput.toUpperCase() } });
-    } else if (searchType === 'projectByName') {
+    console.log("searchType: ", searchType);
+    console.log("queryInput: ", queryInput);
+    if (searchType === "projectsByTechnology") {
+      projectsByTechnology({
+        variables: { technology: queryInput.toUpperCase() },
+      });
+    } else if (searchType === "projectByName") {
       projectByName({ variables: { searchTerm: queryInput } });
-    } else if (searchType === 'userByName') {
+    } else if (searchType === "userByName") {
       userByName({ variables: { searchTerm: queryInput } });
     } else {
-      console.log('Invalid search type.');
+      console.log("Invalid search type.");
     }
   };
 
-
-
   const renderResults = () => {
-    if (searchType === 'GetProjectsbyTechnology' && projectsByTechData?.projectsByTechnology) {
+    if (
+      searchType === "projectsByTechnology" &&
+      projectsByTechData?.getProjectsByTechnology
+    ) {
+      console.log(
+        "projectsByTechData: ",
+        projectsByTechData.getProjectsByTechnology
+      );
       return (
         <ul>
-          {projectsByTechData.projectsByTechnology.map((project) => (
-            <li key={project._id} onClick={() => router.push(`/projects/${project._id}`)}>
+          {projectsByTechData.getProjectsByTechnology.map((project) => (
+            <li
+              key={project._id}
+              onClick={() => router.push(`/projects/${project._id}`)}
+            >
               {project.name} - {project.technologies}
             </li>
           ))}
@@ -44,12 +61,15 @@ const Search = () => {
       );
     }
 
-
-    if (searchType === 'searchProjectByName' && projectsData?.searchProjectByName) {
+    if (searchType === "projectByName" && projectsData?.searchProjectByName) {
+      console.log("projectsData: ", projectsData);
       return (
         <ul>
           {projectsData.searchProjectByName.map((project) => (
-            <li key={project._id} onClick={() => router.push(`/project/${project._id}`)}>
+            <li
+              key={project._id}
+              onClick={() => router.push(`/projects/${project._id}`)}
+            >
               {project.name}
             </li>
           ))}
@@ -57,12 +77,16 @@ const Search = () => {
       );
     }
 
-    if (searchType === 'searchUserByName' && usersData?.searchUserByName) {
+    if (searchType === "userByName" && usersData?.searchUserByName) {
+      console.log("usersData: ", usersData);
       return (
         <ul>
           {usersData.searchUserByName.map((user) => (
-            <li key={user._id} onClick={() => router.push(`/users/${user._id}`)}>
-              {user.firstname}
+            <li
+              key={user._id}
+              onClick={() => router.push(`/users/${user._id}`)}
+            >
+              {user.firstName} {user.lastName.substring(0, 1)}
             </li>
           ))}
         </ul>
@@ -74,10 +98,10 @@ const Search = () => {
 
   return (
     <Box sx={{ padding: 4 }}>
-      <Typography variant="h4" gutterBottom>Search</Typography>
-      <Box sx={{ marginBottom: 2,
-                backgroundColor: '#ffffff'
-       }}>
+      <Typography variant="h4" gutterBottom>
+        Search
+      </Typography>
+      <Box sx={{ marginBottom: 2, backgroundColor: "#ffffff" }}>
         <TextField
           select
           label="Search Type"
@@ -85,7 +109,9 @@ const Search = () => {
           onChange={(e) => setSearchType(e.target.value)}
           fullWidth
         >
-          <MenuItem value="projectsByTechnology">Projects by technology</MenuItem>
+          <MenuItem value="projectsByTechnology">
+            Projects by technology
+          </MenuItem>
           <MenuItem value="projectByName">Projects by Name</MenuItem>
           <MenuItem value="userByName">User by Name</MenuItem>
         </TextField>
@@ -97,13 +123,13 @@ const Search = () => {
           value={queryInput}
           onChange={(e) => setQueryInput(e.target.value)}
           fullWidth
-          sx={{ marginBottom: 2,
-            backgroundColor: '#ffffff'
-           }}
+          sx={{ marginBottom: 2, backgroundColor: "#ffffff" }}
         />
       )}
 
-      <Button variant="contained" onClick={handleSearch}>Search</Button>
+      <Button variant="contained" onClick={handleSearch}>
+        Search
+      </Button>
 
       <Box sx={{ marginTop: 4 }}>
         <Typography variant="h6">Results:</Typography>
