f3fa69d0658ee0f3504800b973388bb3bb2c1f43
Calvin526 <calvinmgarcia@hotmail.com>
2024-12-18 02:48:20 -0500

    Added test auth user to All Projects List component

--- client/src/app/projects/page.jsx
+++ client/src/app/projects/page.jsx
@@ -7,20 +7,35 @@ export default function AllProjects() {
   const { loading, error, data } = useQuery(queries.projects, {
     fetchPolicy: "cache-and-network",
   });
-  if (loading) {
+
+  const { loading: loading2, error: error2, data: data2 } = useQuery(queries.getUserById, {
+    fetchPolicy: "cache-and-network",
+    variables: { id: "000000000000000000000000" },
+  });
+
+
+  if (loading || loading2) 
+  {
     return <div>Loading...</div>;
   }
-  if (error) {
+
+  if (error || error2) 
+  {
     return <div>Error! {error.message}</div>;
-  } else if (data) {
+  } 
+  
+  else if (data && data2) {
     let projects = data.projects;
+
+    let user = data2.getUserById;
+
     return (
       <div className="bg-white ">
         <section className="text-center">
           <h1 className="text-4xl font-bold text-gray-800 mb-4">
             All Projects
           </h1>
-          <AllProjectsList projectList={projects} />
+          <AllProjectsList projectList={projects} currentUser ={user} />
           <div className="flex-auto space-x-4"></div>
         </section>
       </div>
